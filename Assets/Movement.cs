3513af1893b51a04ab59067bf6b5a00a60accc8e
RyanLynar <lyna0001@algonquinlive.com>
2017-01-19 13:59:20 -0500

    New ProjectIdea
    Bunch more fiddling really, movement doing some ice things that I want
    it to

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -2,97 +2,98 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Movement : MonoBehaviour {
-    
-    Vector3 speed;
-    Vector3 accel;
+    public float speedMult;
+    Vector2 speed;
+    Vector2 accel;
+    bool vertAction, horizAction;
 	// Use this for initialization
 	void Start () {
-        speed = new Vector3(0, 0, 0);
+        speed = GetComponent<Rigidbody2D>().velocity;
+        
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update () {
+        keyReleased();
         accel = new Vector3(0, 0, 0);
-        //Collision Check
+        
+        HorizMove();
+        
+        VertMove();
 
-       if (checkCollision(gameObject)) {
-            speed = Vector3.zero;
-       }
-
-        accel = slowDown(accel, speed);
-        //Accleration
+        speed = speed + accel;
+        if (!horizAction)
+        {
+            speed.x = Mathf.Lerp(speed.x, 0, Time.deltaTime * 10000);
+        }
+        if (!vertAction)
+        {
+            speed.y = Mathf.Lerp(speed.y, 0, Time.deltaTime * 10000);
+        }
+        GetComponent<Rigidbody2D>().velocity = speed;
+    }   
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+    }
+    void OnTriggerExit2D(Collider2D coll)
+    {
+        if (gameObject.tag =="Player")
+        {
+            gameObject.transform.localPosition = new Vector3(0, 0, 1);
+        }
+    }
+    public void keyReleased()
+    {
+        if(Input.GetKeyUp("a") || Input.GetKeyUp("d"))
+        {
+            horizAction = false;
+        }
+        if (Input.GetKeyUp("s")|| Input.GetKeyUp("w"))
+        {
+            vertAction = false;
+        }
+    }
+    public void HorizMove() {
         if (Input.GetKey("a"))
         {
-            if(speed.x >= -1) {
-                accel.x -= 0.25f;
-            }else { accel.x = 0; }
-        }
-        else if(Input.GetKey("d")){
-            if (speed.x <= 1)
+            horizAction = true;
+            if (speed.x >= speedMult*-1)
             {
-                accel.x += 0.25f;
+                accel.x -= speedMult * 0.25f;
             }
             else { accel.x = 0; }
         }
-        //Vert Movement
+        else if (Input.GetKey("d"))
+        {
+            horizAction = true;
+            if (speed.x <= speedMult * 1)
+            {
+                accel.x += speedMult * 0.25f;
+            }
+            else { accel.x = 0; }
+        }
+    }
+    public void VertMove() {
+
         if (Input.GetKey("s"))
         {
-            if (speed.y >= -1)
+            vertAction = true;
+            if (speed.y >= speedMult * -1)
             {
-                accel.y -= 0.25f;
-            }
-            else { accel.y = -0; }
-        }
-        else if (Input.GetKey("w"))
-        {
-            if (speed.y <= 1)
-            {
-                accel.y+= 0.25f;
+                accel.y -= speedMult * 0.25f;
             }
             else { accel.y = 0; }
         }
-        speed = speed + accel;
-        if (speed.x > 0.1 || speed.x < -0.1) 
-                    speed.x = 0;
-        transform.Translate(speed);
-
-    }
-    public Vector3 slowDown(Vector3 accel, Vector3 speed)
-    {
-        //Accel Decay
-        
-        //Horizontal
-        if (speed.x > 0) {
-            accel.x -= 0.05f;
-            if (speed.x - accel.x < 0)
-                speed.x = 0;
-        }
-        else if (speed.x < 0) {
-            accel.x += 0.05f;
-            if (speed.x - accel.x > 0)
-                speed.x = 0;
-        }
-
-        //Vert
-        if (speed.y > 0) {
-            accel.y -= 0.05f;
-            if (speed.y - accel.y > 0)
-                speed.y = 0;
-        }
-        else if (speed.y < 0) {
-            accel.y += 0.05f;
-        }
-        
-        return accel;
-    }
-    public bool checkCollision(GameObject mover)
-    {
-        GameObject wall = GameObject.Find("Wall1");
-        if (wall.GetComponent<Collider2D>().IsTouching(mover.GetComponent<Collider2D>()))
+        else if (Input.GetKey("w"))
         {
-            return true;
+            vertAction = true; 
+            if (speed.y <= speedMult * 1)
+            {
+                accel.y += speedMult * 0.25f;
+            }
+            else { accel.y = 0; }
         }
-        return false;
     }
 }
---
+++ Wall.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace ExtraObjects
+{
+    public class Wall 
+    {
+        Vector2 position;
+        int width, height;
+        SpriteRender render;
+        public Wall()
+        {
+            position = new Vector2(0, 0);
+            width = 5;
+            height = 5;
+            render = new SpriteRender();
+         
+        }
+        public Wall(Vector2 pos,int width, int height)
+        {
+            position = pos;
+            this.width = width;
+            this.height = height;
+        }
+    }
+}
